0524d84d77e9553692d739888aa686c278e084f8
Tim Lanzi <trlanzi@gmail.com>
2020-08-07 15:26:30 -0400

    Fix viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,33 @@
+import React, { useEffect } from "react";
 import { AppProps } from 'next/app'
+import Head from "next/head";
 import NProgress from "@/components/nprogress";
 import "@/styles/root.scss"
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme } from "@material-ui/core/styles"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <>
+    <ThemeProvider theme={createMuiTheme()}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <title>Tim Lanzi</title>
+      </Head>
+      <CssBaseline />
       <NProgress />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
